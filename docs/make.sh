25cfdc655d6e8eee89d48862029b96f07cade243
Rémy Oudompheng <remy@archlinux.org>
2011-07-14 12:06:59 +0200

    Documentation overhaul.
    Merge ethernet and wireless into a single netcfg-profiles.txt
    file aimed at gathering documentation for all connection types
    and profile writing.
    
    Signed-off-by: Rémy Oudompheng <remy@archlinux.org>

--- docs/make.sh
+++ docs/make.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-PAGES=(ethernet features wireless)
+PAGES=(features netcfg netcfg-profiles)
 
 make_page() {
     echo '<html><body>'
@@ -7,16 +7,20 @@ make_page() {
     echo '</body></html>'
 }
 
+# HTML page generation
 for page in ${PAGES[@]}; do
     rm -f ${page}.html
     if which pandoc &>/dev/null; then
-        pandoc -s --toc -w html --email-obfuscation=javascript -c header.css -o ${page}.html $page
+        pandoc -s --toc -w html --email-obfuscation=javascript -c header.css -o ${page}.html $page.txt
     else
-        make_page $page > ${page}.html
+        make_page $page.txt > ${page}.html
     fi
 done
 
 # Generate manpages
 if which pandoc &>/dev/null; then
     pandoc -s -w man -o netcfg.8 netcfg.txt
+    pandoc -s -w man -o netcfg-profiles.5 netcfg-profiles.txt
 fi
+
+# vim: set ts=4 sw=4 et tw=0:
