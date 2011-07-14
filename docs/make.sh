0fe676b10a8c13fd8e552f7fb56bc845aac7dd9b
Rémy Oudompheng <remy@archlinux.org>
2011-07-14 11:48:06 +0200

    Auto-generate the netcfg manpage from markdown/pandoc source.
    Signed-off-by: Rémy Oudompheng <remy@archlinux.org>

--- docs/make.sh
+++ docs/make.sh
@@ -9,9 +9,14 @@ make_page() {
 
 for page in ${PAGES[@]}; do
     rm -f ${page}.html
-    if [ -f /usr/bin/pandoc ]; then
+    if which pandoc &>/dev/null; then
         pandoc -s --toc -w html --email-obfuscation=javascript -c header.css -o ${page}.html $page
     else
         make_page $page > ${page}.html
     fi
 done
+
+# Generate manpages
+if which pandoc &>/dev/null; then
+    pandoc -s -w man -o netcfg.8 netcfg.txt
+fi
